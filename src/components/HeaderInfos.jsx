c9908f9b6e6f4429599f21fb115d29f9f7be45cd
Floriane HJ <fjulia.dev@gmail.com>
2024-09-24 12:53:15 +0200

    Correction bug

--- src/components/HeaderInfos.jsx
+++ src/components/HeaderInfos.jsx
@@ -6,19 +6,31 @@ const HeaderInfos = () => {
     const [headerData, setHeaderData] = useState([]);
 
     useEffect(() => {
-        axios.get("https://api.coingecko.com/api/v3/global").then((res) => setHeaderData(res.data.data))
+        axios
+            .get("https://api.coingecko.com/api/v3/global")
+            .then((res) => setHeaderData(res.data.data))
     }, [])
 
     return (
         <div className="header-container">
-            <header>
+            
               <ul className="title">
                 <li>
                     <h1><img src="./assets/logo.png" alt="logo du site" />Watch tower</h1>
                 </li>
-                <li>Crypto-monnaies : {headerData.active_cryptocurrencies.toLocaleString()}</li>
+                <li>Crypto-monnaies : {" "} 
+                {/* Vérifier que la donnée existe bien */}
+                {headerData.active_cryptocurrencies &&
+             headerData.active_cryptocurrencies.toLocaleString()}
+                </li>
+                <li>Marchés : {headerData.markets && headerData.markets}</li>
               </ul>
-            </header>
+              <ul className="infos-mkt">
+                <li className="global-mkt">
+                    Global Market Cap : 
+                </li>
+              </ul>
+            
         </div>
 
     );
