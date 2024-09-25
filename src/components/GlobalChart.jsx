26fbe2ef010498ef57e8efacdb7066bbcd409bc3
Floriane HJ <fjulia.dev@gmail.com>
2024-09-25 10:16:50 +0200

    Ajout GlobalChart.jsx

---
+++ src/components/GlobalChart.jsx
@@ -0,0 +1,39 @@
+import React, { useEffect, useState } from 'react';
+import { Treemap } from 'recharts';
+
+const GlobalChart = ({ coinsData }) => {
+    
+    const [dataArray, setDataArray] = useState([]);
+
+    useEffect(() => {
+        let chartData = [];
+
+        if (coinsData.length > 0) {
+            for (let i = 0; i < 45; i ++) {
+                chartData.push({
+                    name: coinsData[i].symbol.toUpperCase() + " " + coinsData[i].market_cap_change_percentage_24h.toFixed(1) + "%",
+                    size: coinsData[i].market_cap,
+                    fill: null,
+
+                })
+            }
+        }
+        setDataArray(chartData);
+        
+    }, [coinsData]);
+    
+    return (
+        <div className="global-chart">
+            <Treemap 
+            width={730}
+            height={181}
+            data={dataArray}
+            dataKey="size"
+            stroke="rgb(51,51,51)"
+            fill='black'
+            />
+        </div>
+    );
+};
+
+export default GlobalChart;
